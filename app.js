3b80fb7b42f980bfd0a0b9fbf83906d83a5ee5b1
Magyar Melinda Barbara <m4gy4r.m3l1nd4@gmail.com>
2024-10-11 23:15:10 +0000

    chore: reformat

--- app.js
+++ app.js
@@ -1,36 +1,36 @@
 const player1 = {
   score: 0,
-  button: document.querySelector('#btn1'),
-  display: document.querySelector('#player1')
-}
+  button: document.querySelector("#btn1"),
+  display: document.querySelector("#player1"),
+};
 
 const player2 = {
   score: 0,
-  button: document.querySelector('#btn2'),
-  display: document.querySelector('#player2')
-}
+  button: document.querySelector("#btn2"),
+  display: document.querySelector("#player2"),
+};
 
-const welcomeDiv = document.querySelector('#welcome');
-const input1 = document.querySelector('#p1Name.input');
-const input2 = document.querySelector('#p2Name.input');
-const playBtn = document.querySelector('#play');
+const welcomeDiv = document.querySelector("#welcome");
+const input1 = document.querySelector("#p1Name.input");
+const input2 = document.querySelector("#p2Name.input");
+const playBtn = document.querySelector("#play");
 
-const gameDiv = document.querySelector('#game');
-const winScore = document.querySelector('#winningScore');
-const resetBtn = document.querySelector('#reset');
-const newBtn = document.querySelector('#new');
+const gameDiv = document.querySelector("#game");
+const winScore = document.querySelector("#winningScore");
+const resetBtn = document.querySelector("#reset");
+const newBtn = document.querySelector("#new");
 
 let winningScore = 3;
 
-playBtn.addEventListener('click', () => {
-  welcomeDiv.style.display = 'none';
-  gameDiv.style.display = 'block';
-})
+playBtn.addEventListener("click", () => {
+  welcomeDiv.style.display = "none";
+  gameDiv.style.display = "block";
+});
 
-winScore.addEventListener('change', function() {
+winScore.addEventListener("change", function () {
   winningScore = parseInt(this.value);
   reset();
-})
+});
 
 function scoreHandler(player, opponent) {
   player.score++;
@@ -38,35 +38,35 @@ function scoreHandler(player, opponent) {
 
   if (player.score === winningScore) {
     player.button.disabled = true;
-    player.display.style.color = 'green';
+    player.display.style.color = "green";
     opponent.button.disabled = true;
-    opponent.display.style.color = 'red';
+    opponent.display.style.color = "red";
   }
 }
 
-btn1.addEventListener('click', () => {
+btn1.addEventListener("click", () => {
   scoreHandler(player1, player2);
-})
+});
 
-btn2.addEventListener('click', () => {
+btn2.addEventListener("click", () => {
   scoreHandler(player2, player1);
-})
+});
 
-resetBtn.addEventListener('click', reset); 
+resetBtn.addEventListener("click", reset);
 
 function reset() {
   for (player of [player1, player2]) {
     player.score = 0;
     player.display.textContent = 0;
     player.button.disabled = false;
-    player.display.style.color = 'black';
+    player.display.style.color = "black";
   }
 }
 
-newBtn.addEventListener('click', () => {
-  welcomeDiv.style.display = 'block';
-  gameDiv.style.display = 'none';
-  input1.value = '';
-  input2.value = '';
-  reset()
-})
+newBtn.addEventListener("click", () => {
+  welcomeDiv.style.display = "block";
+  gameDiv.style.display = "none";
+  input1.value = "";
+  input2.value = "";
+  reset();
+});
